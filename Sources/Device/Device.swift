cf0c82927a30274833bcffea87f967f7af472cbc
Mykhailo Tymchyshyn <mykhailo.tymchyshyn@gmail.com>
2025-03-23 23:56:45 +0200

    UART TX

--- Sources/Device/Device.swift
+++ Sources/Device/Device.swift
@@ -1,5 +1,11 @@
+/// Clock control
+nonisolated(unsafe) public let clock = CLOCK(unsafeAddress: 0x40000000)
+
 /// GPIO Port 1
 nonisolated(unsafe) public let p0 = P0(unsafeAddress: 0x50000000)
 
 /// GPIO Port 2
 nonisolated(unsafe) public let p1 = P1(unsafeAddress: 0x50000300)
+
+/// Universal Asynchronous Receiver/Transmitter
+nonisolated(unsafe) public let uart0 = UART0(unsafeAddress: 0x40002000)
--- Sources/microbit/microbit.swift
+++ Sources/microbit/microbit.swift
@@ -11,7 +11,11 @@ struct Game {
             rw.raw.dir = 1
         }
 
+        enableClock()
+        serialInit()
+        
         var isOn = false
+        var counter = 48
         while true {
             p0.out.modify { rw in
                 rw.raw.pin21 = isOn ? 1 : 0
@@ -20,6 +24,52 @@ struct Game {
                 nop()
             }
             isOn = !isOn
+            counter += 1
+            serialPutc(UInt8(counter))
         }
     }
+
+    static func enableClock() {
+        clock.tasks_hfclkstop.modify { r, w in
+            w.tasks_hfclkstop_field = .Trigger
+        }
+        clock.tasks_hfclkstart.modify { r, w in
+            w.tasks_hfclkstart_field = .Trigger
+        }
+        while clock.events_hfclkstarted.read().events_hfclkstarted_field != .Generated {}
+    }
+
+    static func serialInit() {
+        uart0.enable.write { rw in
+            rw.enable_field = .Disabled
+        }
+        uart0.baudrate.write { rw in
+            rw.baudrate_field = .Baud9600
+        }
+        uart0.config.modify { rw in
+            rw.parity = .Excluded
+        }
+        uart0.psel.txd.modify { rw in
+            rw.raw.port = 0
+            rw.raw.pin = 6
+            rw.connect = .Connected
+        }
+        uart0.enable.write { rw in
+            rw.enable_field = .Enabled
+        }
+
+        uart0.tasks_starttx.modify { r, w in
+            w.tasks_starttx_field = .Trigger
+        }
+    }
+
+    static func serialPutc(_ char: UInt8) {
+        
+        uart0.txd.modify { r, w in
+            w.raw.txd_field = UInt32(char)
+        }
+
+        while uart0.events_txdrdy.read().events_txdrdy_field == .NotGenerated {}   
+    }
 }
+
